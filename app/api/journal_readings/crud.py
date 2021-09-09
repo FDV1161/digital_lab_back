4522ca07788a77347f8f80991dd0846e8f3f9833
fisherdv <fisherd@mail.ru>
2021-09-09 23:06:16 +0600

    сортировка журнала показаний

--- app/api/journal_readings/crud.py
+++ app/api/journal_readings/crud.py
@@ -1,8 +1,17 @@
 from app.api.base.functions import BaseCRUD
 from app.api.journal_readings.filter import JournalReadingsFilter
 from app.models import JournalReadings
+from database import session
 
 
 class JournalReadingsCRUD(BaseCRUD):
     model = JournalReadings
     filter = JournalReadingsFilter
+
+    def get_items(self, filters = None, page=1, count=10):        
+        stmt = self.model.query
+        if self.filter:
+            stmt = self.filter().filter_query(stmt, filters.dict(exclude_unset=True))        
+        row_count = stmt.count()
+        stmt = stmt.order_by(self.model.id.desc())
+        return stmt.paginate(page, count, False).items, row_count
